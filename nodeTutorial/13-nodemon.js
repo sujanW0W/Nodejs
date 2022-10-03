983d0512f946c385088d9a3bbdd2f69577f13ccf
Sujan Maharjan <87776506+sujanW0W@users.noreply.github.com>
2022-10-03 21:46:45 +0545

    Completion of first phase.

--- nodeTutorial/13-nodemon.js
+++ nodeTutorial/13-nodemon.js
@@ -1,4 +1,4 @@
-//nodemon - it watches the file. If made some changes, it will execute it immediately. So, I wont need to "node app.js" everytime.
+//nodemon - it watches the file. If some changes are detected, it will automatically restart the application immediately. So, I wont need to "node app.js" everytime.
 
 //The "scripts" in package.json
 //Basically, we set up the scirpts and run it using "npm [command]"
@@ -9,6 +9,10 @@
 //devDependencies means those that will be used during development stage, not during the production.
 //a script for nodemon is set.
 
-const smth = "Hello People"
+const smth = "Hello World"
 
 console.log(smth)
+
+//Global dependencies
+//The dependencies can be installed globally. 
+//By installing the packages globally, it can be used by multiple projects.
