b2b8e7a40e9e5e990db485ef38e8f8e4050f00b4
Sujan Maharjan <87776506+sujanW0W@users.noreply.github.com>
2022-10-26 23:56:42 +0545

    Learnt to connect the DB.

--- 04-DB_Tutorial/starter/DB/connect.js
+++ 04-DB_Tutorial/starter/DB/connect.js
@@ -1,1 +1,7 @@
-const connectionString = "mongodb+srv://sujanW0W:<password>@nodeprojects.t1xaot0.mongodb.net/?retryWrites=true&w=majority"
+const mongoose = require('mongoose')
+
+const connectDB = (uri) => {
+    return mongoose.connect(uri)
+}
+
+module.exports = connectDB
--- 04-DB_Tutorial/starter/app.js
+++ 04-DB_Tutorial/starter/app.js
@@ -6,6 +6,11 @@ const app = express();
 //middleware
 app.use(express.json())
 
+const connectDB = require('./DB/connect')
+
+require('dotenv').config()
+
+
 //routes
 
 const tasks = require('./routes/tasks');
@@ -14,6 +19,19 @@ app.use('/api/v1/tasks', tasks)
 
 
 const port = 3000;
-app.listen( port, () => {
-    console.log(`Server is running on port ${port}.....`);
-})
+
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGO_URI);
+
+        app.listen( port, () => {
+            console.log(`Server is running on port ${port}.....`);
+        })
+    }
+    catch(err){
+        console.log("ERROR")
+    }
+}
+
+start()
+
