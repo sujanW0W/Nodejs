31ee461df1ffb14d95f43506d78377bb54ba2300
Sujan Maharjan <87776506+sujanW0W@users.noreply.github.com>
2022-11-07 15:29:24 +0545

    Learnt Sorting, Searching, Filter, Pagination, and so forth.

---
+++ 06-storeAPI/ReadMe.txt
@@ -0,0 +1,12 @@
+
+In this project, I learnt about different functionaities that MongoDB and Mongoose(npm) provide.
+
+I learnt to search, sort, pagination, and so forth.
+
+Search on the basis of exact name, some characters that exist in the name using Regular Expression.
+
+Sort in ascending and descending order, numeric filter - items having numeric value of some sort(greater, less, equal) using Regular Expression.
+
+Pagination - Limit items per page, Skip number of items based on page number.
+
+Filter the data to be sent - providing only requested fields of the document.
--- 06-storeAPI/controller/products.js
+++ 06-storeAPI/controller/products.js
@@ -21,7 +21,7 @@ const getAllProducts = async (req, res, next) => {
     //The strict is true by default, this means that the queries that does not exist in our schema gets ignored by default.
     //However, the robust method to do so is:
 
-    const { featured, company } = req.query //We have to get only those strings that exist in our schema.
+    const { featured, company, name, sort, field, numericFilters } = req.query //We have to get only those strings that exist in our schema.
     const queryObject = {}
 
     // IMPORTANT - Query params are received as String. So, the featured received is String. So, it must be compared with a string like done above.
@@ -34,7 +34,78 @@ const getAllProducts = async (req, res, next) => {
         queryObject.company = company
     }
 
-    const products = await Products.find(queryObject)
+    if (name) {
+        queryObject.name = { $regex: name, $options: "i" }
+    }
+
+    if (numericFilters) {
+        const operatorMap = {
+            ">": "$gt",
+            ">=": "$gte",
+            "=": "$eq",
+            "<": "$lt",
+            "<=": "$lte",
+        }
+
+        const regEx = /\b(<|<=|=|>|>=)\b/g
+
+        let filters = numericFilters.replace(
+            regEx,
+            (match) => `-${operatorMap[match]}-`
+        )
+
+        filters = filters.split(",")
+
+        const options = ["price", "rating"]
+
+        filters.forEach((item) => {
+            const [field, operator, value] = item.split("-")
+
+            if (options.includes(field)) {
+                if (field in queryObject) {
+                    queryObject[field][operator] = Number(value)
+                    return //Equivalent to continue in for loop.
+                }
+
+                queryObject[field] = { [operator]: Number(value) }
+            }
+        })
+    }
+    console.log(queryObject)
+    // const products = await Products.find(queryObject)
+
+    //Sorting is done. If specific field for sorting is provided, then that particular field will be used for sorting, else sorting will be done based on creation time(cratedAt).
+    //Since the sort() should be chained with the find() request, if await is done during find(), then the functionality wont work.
+    //This is because, if waited for the find() to be completed, we will wait for the result of the find operation. After getting the result of the find() operation, sort() will not work because it is not chained.
+    //So, in order to make sort() work, we have to chain the sort() before the find() is completed.
+    //For this, We will not awati for the find() and chain the sort() with the find, before it gets completed. We can await for the  result not while finding but while waiting for the sorted result.
+
+    let result = Products.find(queryObject)
+    if (sort) {
+        //In URL, if there are multiple sorting fields, then they are provided by separating with comma.
+        //But in sort(), they must be provided by separating with empty space. So, we need to replace comma with an empty space.
+
+        let sortList = sort.split(",").join(" ")
+        result = result.sort(sortList)
+    } else {
+        result = result.sort("createdAt")
+    }
+
+    //Selecting some specific fields only. select()
+    if (field) {
+        let fieldList = field.split(",").join(" ")
+        result = result.select(fieldList)
+    }
+
+    //Pagination
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    let skip = (page - 1) * limit
+    result = result.skip(skip).limit(limit)
+
+    const products = await result
+
     res.status(200).json({ products, nbHits: products.length })
 }
 
